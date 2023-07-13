591e6bdd6d30be3b1defa70216be6ac9668bd002
planimir77 <planimir7708@gmail.com>
2023-07-13 11:28:31 +0300

    update: add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -7,9 +8,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '475px',
+      'smmax': {'max': '639px'},
+      ...defaultTheme.screens,
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
   ],
 }
-
