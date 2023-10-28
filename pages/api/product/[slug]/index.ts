42c45e5fde80b2ae4f671a4205cf82d303bc220e
planimir77 <planimir7708@gmail.com>
2023-10-28 10:54:09 +0300

    feat: add product api

---
+++ pages/api/product/[slug]/index.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as cheerio from "cheerio";
+
+type Data = {
+  slug: string;
+};
+
+export default async function GET(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  debugger;
+  const URL = `https://www.si-nature.com/products/${req.query.slug}`;
+  const html = await fetch(URL, { next: { revalidate: 0 } })
+    .then(function (response) {
+      // The API call was successful!
+      return response.text();
+    })
+    .then(function (html) {
+      // This is the HTML from our response as a text
+      return html;
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+
+  if (!html) return res.status(404);
+
+  const $ = cheerio.load(html as unknown as string);
+  const element = $("#ProductJson-product-template")[0];
+  const text = $(element).text();
+  const obj = JSON.parse(text);
+
+  res.status(200).json(obj);
+}
