e605b0e9de49b237283605e7b5e76556bc4806d8
planimir77 <planimir7708@gmail.com>
2023-11-05 17:06:43 +0200

    fix: get products async

--- components/ui/CollectionPreview/CollectionPreview.tsx
+++ components/ui/CollectionPreview/CollectionPreview.tsx
@@ -1,16 +1,18 @@
 import cn from "clsx";
 import Link from "next/link";
 import s from "./CollectionPreview.module.css";
-import React, { FC } from "react";
+import React from "react";
 import Chevron from "../Chevron/Chevron";
 import { toCapitalCase } from "utils/stringToCapitalCase";
 import ProductsGrid from "components/product/ProductsGrid/ProductsGrid";
+import { getProducts } from "utils/getProducts";
 
 interface Props {
   collection: Collection;
 }
 
-const CollectionPreview: FC<Props> = ({ collection }) => {
+async function CollectionPreview({ collection }: Props) {
+  const products = await getProducts(collection.productsHandle);
   return (
     <div className="py-6 w-full lg:max-w-5xl md:mx-auto">
       <div className="grid grid-cols-1 sm:grid-cols-[1fr_150px] mb-1">
@@ -27,14 +29,14 @@ const CollectionPreview: FC<Props> = ({ collection }) => {
       </div>
       <hr />
       <span className="block sm:hidden text-sm text-slate-600 leading-8">
-        <Link className="inline-flex" href={collection.link + '?page=1'}>
+        <Link className="inline-flex" href={collection.link + "?page=1"}>
           See more
           <Chevron className="h-[11px] -rotate-90 translate-x-[10px] translate-y-[11px]" />
         </Link>
       </span>
-      <ProductsGrid products={collection.products} />
+      {products && <ProductsGrid products={products} />}
     </div>
   );
-};
+}
 
-export default CollectionPreview;
+export default CollectionPreview as unknown as (props: Props) => JSX.Element;
