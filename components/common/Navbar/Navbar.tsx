8d485eff649792eb6d853b1aa7d49e6b7dfe6946
planimir77 <planimir7708@gmail.com>
2023-11-12 20:14:10 +0200

    fix: change link with button

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -23,16 +23,15 @@ const Navbar: FC<Props> = ({ className }) => {
       <div
         className={`${s.mobileNav} items-center sm:hidden sticky grid grid-cols-2 h-16 top-0 bg-white  shadow-emerald-950/20 shadow-lg z-10 py-4 px-7`}
       >
-        <Link
+        <button
         className="w-fit"
           role="button"
           title={`${isMenuOpen ? "Close" : "Open"} Menu`}
-          href={""}
           onClick={menuToggle}
         >
           <RxHamburgerMenu className="fill-emerald-950 h-9 w-9" />
           <span className="hidden">Menu</span>
-        </Link>
+        </button>
         <UserCartBtn className={cn(s.cart)}></UserCartBtn>
       </div>
       <nav
