4b09ff41e8c916d116c4c537d49cf086398e335d
planimir77 <planimir7708@gmail.com>
2023-11-12 20:15:19 +0200

    update: add heading

--- components/common/OnFocus/OnFocus.tsx
+++ components/common/OnFocus/OnFocus.tsx
@@ -1,18 +1,21 @@
+import cn from "clsx";
+import s from "./OnFocus.module.css";
 import ProductCard from "components/product/ProductCard/ProductCard";
 import React from "react";
-
 
 interface Props {
   onFocusProducts?: Product[];
 }
 
-export default function OnFocus({onFocusProducts: products}: Props) {
+export default function OnFocus({ onFocusProducts: products }: Props) {
   return (
     <>
-      {products && products.map((data) => (
-            <ProductCard key={data.id} product={data}></ProductCard>
-          )
-      )}
+      <h1 className={cn(s.heading)}>On Focus</h1>
+      <hr />
+      {products &&
+        products.map((data) => (
+          <ProductCard key={data.id} product={data}></ProductCard>
+        ))}
       {!products && <div>No Products</div>}
     </>
   );
