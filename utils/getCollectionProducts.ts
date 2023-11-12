df3952fb633cab73131da522144daf7a82c7e28a
planimir77 <planimir7708@gmail.com>
2023-11-12 20:19:16 +0200

    fix: data fetching

--- utils/getCollectionProducts.ts
+++ utils/getCollectionProducts.ts
@@ -1,12 +1,13 @@
-export const getCollectionProducts = async (queryData: CollectionQueryData) => {
-  const collectionData: Promise<CollectionData> =
-    apiGetCollectionProducts(queryData);
-  const collection = await Promise.resolve(collectionData);
+import clientPromise from "@lib/mongodb";
 
-  return collection;
+type keyStringArray = {
+  [key: string]: { [key: string]: string }[];
+};
+type keyString = {
+  [key: string]: { [key: string]: string };
 };
 
-const apiGetCollectionProducts = async (queryData: CollectionQueryData) => {
+export const getCollectionProducts = async (queryData: CollectionQueryData) => {
   const [tag, sortQuery] = queryData.query // farbe-braun&price-descending
     ? queryData.query.split("&")
     : ["", ""];
@@ -14,12 +15,118 @@ const apiGetCollectionProducts = async (queryData: CollectionQueryData) => {
     ? sortQuery.split("-")
     : ["", ""];
 
-  const url = `${process.env.API_URL}/api/collection/${queryData.collectionName}?page=${queryData.page}&tag=${tag}&sort_by=${sortBy}&sorting_order=${sortingOrder}&products_on_page=${queryData.products_on_page}`;
+  const page = Number(queryData.page) || 1;
+  const productsOnPage: number = Number(queryData.products_on_page) || 50;
+  const collection = queryData.collectionName?.toString() || "";
+  const collectionFilter = getCollectionFilter(collection);
+  const tagFilter = getTagFilter(tag); //tags: 'Farbe Blau'
+  try {
+    const skip = productsOnPage * (page - 1);
 
-  const response = await fetch(url, { next: { revalidate: 10 } });
+    const client = await clientPromise;
+    // =========== Database Name ============
+    const db = client.db("eco-natural-senses");
+    let doc;
+    let count: number;
+    // const tags: string[] = await db.collection("products").distinct("tags", { $or: collectionFilter });
+    const tags: string[] = await db.collection("products").distinct("tags");
+    if (tagFilter && sortBy) {
+      doc = await db
+        .collection("products")
+        .find({ $and: [{ $or: collectionFilter }, { $and: [tagFilter] }] })
+        //.sort({ [sortBy]: sortingOrder })
+        //.sort({ "variants.id": 1 })
+        .skip(skip)
+        .limit(productsOnPage)
+        .toArray();
+      count = await db.collection("products").countDocuments({
+        $and: [{ $or: collectionFilter }, { $and: [tagFilter] }],
+      });
 
-  if (!response) return null;
-  const data = await response.json();
+      console.log(`BY Tags AND SORT BY: ${sortBy}`);
+    } else if (sortBy) {
+      doc = await db
+        .collection("products")
+        .find({ $or: collectionFilter })
+        //.sort({ [sortBy]: sortingOrder }) //.sort({ 'variants.id': 1 })
+        .skip(skip)
+        .limit(productsOnPage)
+        .toArray();
+      count = await db
+        .collection("products")
+        .countDocuments({ $or: collectionFilter });
+      console.log(`SORT BY: ${sortBy}`);
+    } else if (tagFilter) {
+      doc = await db
+        .collection("products")
+        .find({ $and: [{ $or: collectionFilter }, { $and: [tagFilter] }] })
+        //.find({ $or: collection }).filter(tag)
+        //.sort({ [sort]: direction === "descending" ? -1 : 1 })
+        //.sort({ "variants.id": 1 })
+        .skip(skip)
+        .limit(productsOnPage)
+        .toArray();
+      count = await db.collection("products").countDocuments({
+        $and: [{ $or: collectionFilter }, { $and: [tagFilter] }],
+      });
+      console.log(`BY Tags: ${sortBy}`);
+    } else {
+      doc = await db
+        .collection("products")
+        .find({ $or: collectionFilter })
+        .skip(skip)
+        .limit(productsOnPage)
+        .toArray();
+      count = await db
+        .collection("products")
+        .countDocuments({ $or: collectionFilter });
+      console.log("WITHOUT SORT");
+    }
+    // { type: { $in: type } },
 
-  return data;
+    const pages = Math.ceil(count / productsOnPage);
+    const products: Product[] = JSON.parse(JSON.stringify([...doc]));
+
+    return { products, collection, pages, tags };
+  } catch (error) {
+    console.error(error);
+    return { error: "Not Found" };
+  }
 };
+
+export function getCollectionFilter(collectionName: string) {
+  const collection: keyStringArray = {
+    "earth-matters": [{ tags: "Earth Matters" }],
+    "sommer-2023": [{ tags: "Sommer 2023" }],
+    "from-holland": [{ tags: "Holland" }],
+    new: [{ tags: "NEU" }],
+    "stabilisierter-amarant": [{ tags: "stabilisierter-amarant" }],
+    "dried-flowers": [
+      { type: "Getrocknete Blumen" },
+      { type: "Craspedia getrocknet" },
+      { type: "Echinops getrocknet" },
+    ],
+    "helichrysum-strohblumen-capblumen": [
+      { type: "Helichrysum" },
+      { type: "Capblumen" },
+    ],
+  };
+  const result = collection[collectionName];
+  return result;
+}
+export function getTagFilter(filter: string | undefined) {
+  const map = new Map([
+    ["farbe-braun", { tags: "Farbe Braun" }],
+    ["farbe-blau", { tags: "Farbe Blau" }],
+  ]);
+  // map.set("farbe-braun", { tags: "Farbe Braun" })
+  // map.set("farbe-blau", { tags: "Farbe Blau" },)
+
+  //   const tag: keyString = {
+  //     "farbe-braun": { tags: "Farbe Braun" },
+  //     "farbe-blau": { tags: "Farbe Blau" },
+  //   };
+  //   const result = filter ? tag[filter] : undefined;
+  const result = filter ? map.get(filter) : undefined;
+  return result;
+}
--- utils/getProduct.ts
+++ utils/getProduct.ts
@@ -1,9 +1,28 @@
-export const getProduct = async (handle:string) => {
-    
-    const response = await fetch(`${process.env.API_URL}/api/product/${handle}`,  { next: { revalidate: 0 } })
+import * as cheerio from "cheerio";
 
-    if (!response) return null
-    const data = await response.json()
+export const getProduct = async (handle:string) => {
+
+    const URL = `${process.env.SI_NATURE}/products/${handle}`;
+    const html = await fetch(URL, { cache: 'no-store' })
+      .then(function (response) {
+        // The API call was successful!
+        return response.text();
+      })
+      .then(function (html) {
+        // This is the HTML from our response as a text
+        return html;
+      })
+      .catch(function (err) {
+        // There was an error
+        console.warn("Something went wrong.", err);
+      });
+  
+    if (!html) return 'Not found';
+  
+    const $ = cheerio.load(html as unknown as string);
+    const element = $("#ProductJson-product-template")[0];
+    const text = $(element).text();
+    const data = JSON.parse(text);
     
     return data
 }
